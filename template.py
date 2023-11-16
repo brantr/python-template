331dfb79d664809f2cda3d54a8d6fec3ee499917
Brant Robertson <brantr@users.noreply.github.com>
2023-11-16 11:02:42 -0800

    Create template.py
    Create a template python script to avoid copying so many things over and over again that I use all the time.

---
+++ template.py
@@ -0,0 +1,52 @@
+#import sys
+#import os
+import numpy as np
+#import matplotlib.pyplot as plt
+#from astropy.io import fits
+#from astropy.wcs import WCS
+import argparse
+#from tqdm import tqdm
+import time
+
+#######################################
+# Create command line argument parser
+#######################################
+
+def create_parser():
+
+	# Handle user input with argparse
+    parser = argparse.ArgumentParser(
+        description="Detection flags and options from user.")
+
+    parser.add_argument('-v', '--verbose',
+                dest='verbose',
+                action='store_true',
+                help='Print helpful information to the screen? (default: False)',
+                default=False)
+
+    return parser
+
+#######################################
+# main() function
+#######################################
+def main():
+
+    #begin timer
+    time_global_start = time.time()
+
+    #create the command line argument parser
+    parser = create_parser()
+
+    #store the command line arguments
+    args   = parser.parse_args()
+
+    #end timer
+    time_global_end = time.time()
+    if(args.verbose):
+    	print(f"Time to execute program: {time_global_end-time_global_start}s.")
+
+#######################################
+# Run the program
+#######################################
+if __name__=="__main__":
+	main()
